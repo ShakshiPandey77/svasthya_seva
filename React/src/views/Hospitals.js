1ebbf92feab091d70c6303990b5079043f7d7903
aishwarya-gowri <aishwarya-gowri1511@gmail.com>
2020-07-16 20:05:11 +0530

    loaction filter added

--- React/src/views/Hospitals.js
+++ React/src/views/Hospitals.js
@@ -4,50 +4,68 @@ import React from "react";
 import HeroFull from "../components/sections/HeroFull03";
 import HospitalTable from "../components/sections/temp";
 import axios from "axios";
-const haversine = require("haversine")
+
+const haversine = require("haversine");
 // import HospitalTable from "../components/sections/HospitalTable";
 class Hospitals extends React.Component {
   constructor() {
     super();
     this.state = {
       data: [],
-        latitude: 0,
-        longitude: 0
+      latitude: 0.0,
+      longitude: 0.0,
     };
   }
-  
+
   componentDidMount() {
     // this.setState({ loading: true });
-    
-      navigator.geolocation.getCurrentPosition(function(position) {
-        var latlon = String(position.coords.latitude + "," + position.coords.longitude);
-        // this.setState(
-        //   {
-        //     latitude: position.coords.latitude,
-        //     longitude: position.coords.longitude
-        //   }
-        // )
-       
-        // this.state.latitude = position.coords.latitude;
-        // this.state.longitude = position.coords.longitude;
-        
-        console.log("Latitude is :",latlon.split(",")[0]);
-        console.log("Longitude is :", latlon.split(",")[1]);
+
+    navigator.geolocation.getCurrentPosition((position) => {
+      var latlon = String(
+        position.coords.latitude + "," + position.coords.longitude
+      );
+      // console.log("latlon", latlon);
+      var lat = position.coords.latitude;
+      var lon = position.coords.longitude;
+      console.log(lat, "lat");
+      console.log(lon, "lon");
+
+      this.setState({
+        latitude: lat,
+        longitude: lon,
       });
-     
-      console.log(this.state.latitude)
-    
+
+      // console.log(this.state.latitude);
+      // console.log(this.state.longitude);
+      // this.state.latitude = position.coords.latitude;
+      // this.state.longitude = position.coords.longitude;
+
+      console.log("Latitude is :", latlon.split(",")[0]);
+      console.log("Longitude is :", latlon.split(",")[1]);
+    });
+
+    console.log(this.state.latitude);
+
     axios
       .get(
         `https://iphy6zyd24.execute-api.ap-south-1.amazonaws.com/test1/hospital`
       )
       .then((res) => {
         let data = res.data;
-        for (var i = 0; i < data.length; i++){
-
-          data[i]["distance"] = Math.round(haversine({ latitude: this.state.latitude, longitude: this.state.longitude },
-          {latitude: Number(data[i].latitude),
-            longitude: Number(data[i].longitude)})*100)/100
+        for (var i = 0; i < data.length; i++) {
+          data[i]["distance"] =
+            Math.round(
+              haversine(
+                {
+                  latitude: this.state.latitude,
+                  longitude: this.state.longitude,
+                },
+                {
+                  latitude: Number(data[i].latitude),
+                  longitude: Number(data[i].longitude),
+                }
+              ) * 100
+            ) / 100;
         }
         this.setState({
           data,
@@ -81,7 +99,7 @@ class Hospitals extends React.Component {
 
   render() {
     const rows = this.state.data;
-    console.log("rows")
+    console.log("rows");
     console.log(rows);
     return (
       <React.Fragment>
