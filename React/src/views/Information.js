d2a52e62642789c221fac02c5821c5feec807d0d
sanjanapatelh <sanjanapatel.cs17@gmail.com>
2020-07-16 20:07:31 +0530

    Awareness Page added

--- React/src/views/Information.js
+++ React/src/views/Information.js
@@ -2,51 +2,71 @@ import React from 'react';
 // import section header
 import SectionHeader from '../components/sections/partials/SectionHeader';
 // import sections
-import HeroFull from '../components/sections/HeroFull02';
+
+// import sections
+import AwarenessTag from '../components/sections/AwarenessTag';
 import Clients from '../components/sections/Clients';
-import Team from '../components/sections/Team';
 
 import GenericSection from '../components/sections/GenericSection';
-import Roadmap from '../components/sections/Roadmap';
-import Cta from '../components/sections/Cta';
-import Steps from '../components/sections/Steps';
-//import Roadmap1 from '../components/sections/Roadmap1';
+import Flowchart from '../components/sections/Flowchart';
 
 // import some required elements
-import Image from '../components/elements/Image';
-import Input from '../components/elements/Input';
+
 import ButtonGroup from '../components/elements/ButtonGroup';
 import Button from '../components/elements/Button';
-import Modal from '../components/elements/Modal';
+import Cta from '../components/sections/Cta';
+
+//import FeaturesTiles from "../FeaturesTiles";
+
 import Accordion from '../components/elements/Accordion';
 import AccordionItem from '../components/elements/AccordionItem';
 
-
+import Symptoms from "../components/sections/Symptoms";
+import Questions from "../components/sections/Questions";
 
 class Information extends React.Component {
 
   state = {
-    demoModalActive: false
+
+
   }
 
-  openModal = (e) => {
-    e.preventDefault();
-    this.setState({ demoModalActive: true });
-  }
-
-  closeModal = (e) => {
-    e.preventDefault();
-    this.setState({ demoModalActive: false });
-  }
 
   render() {
 
+    const genericSection01Header = {
+      title: 'All you need to know about COVID..'
+    }
 
 
     return (
       <React.Fragment>
 
-      <Steps hasBgColor invertColor />
+      < AwarenessTag hasBgColor invertColor  />
+
+
+
+      <GenericSection
+      >
+          <Questions  />
+
+      </GenericSection>
+
+
+
+      <GenericSection hasBgColor invertColor topDivider >
+
+
+
+            <Symptoms invertMobile  image />
+
+    
+      </GenericSection>
+
+
+
+
+
       </React.Fragment>
     );
   }
